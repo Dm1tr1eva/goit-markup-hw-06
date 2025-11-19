3069916be513f840b439aa188ad47784d469e037
Daryna <dmitrievadasa15@gmail.com>
2025-11-19 18:33:20 +0100

    add tablet styles

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,16 @@ function toggleMenu() {
 
 btnMenu.addEventListener('click', toggleMenu);
 btnClose.addEventListener('click', toggleMenu);
+
+const btnHero = document.querySelector('.hero__button');
+const modal = document.querySelector('.modal-overlay');
+const btnCloseModal = document.querySelector('.modal__button-close');
+
+function toggleModal() {
+  modal.classList.toggle('is-open');
+  btnHero.classList.toggle('hidden');
+  btnCloseModal.classList.toggle('hidden');
+}
+
+btnHero.addEventListener('click', toggleModal);
+btnCloseModal.addEventListener('click', toggleModal);
